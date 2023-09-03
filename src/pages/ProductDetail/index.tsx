dc9f40b4f1b9dc2e324dea06ca4bb56edfb6b857
river221 <hanriver221@gmail.com>
2023-09-03 20:12:21 +0900

    :sparkles: 상품 정렬 & 상품 구매 옵션 선택

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -4,14 +4,43 @@ import { useQuery } from '@tanstack/react-query';
 import fetcher from '../../utilities/fetcher';
 import { baseUrl } from '../ProductList';
 import { Products } from '../../type';
+import { useState } from 'react';
 
 const ProductDetail = () => {
   const { id } = useParams();
+  const today = new Date().toLocaleDateString().replaceAll('.', '').split(' ');
+
+  const [isSelect, setIsSelect] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDeliveryTimes = (times: string[]) => {
+    if (times.length < 1) return 2;
+    const total = times.reduce((acc, cur) => Number(acc) + Number(cur), 0);
+    return Math.ceil(total / times.length);
+  };
+
+  const handleArrivalDate = (times: string[]) => {
+    const today = new Date().getTime();
+    const period = handleDeliveryTimes(times);
+    const deliveryTime = 1000 * 60 * 60 * 24 * period;
+    return new Date(today + deliveryTime).toLocaleDateString();
+  };
 
   const { data: product, isLoading } = useQuery(['product', id], async () => {
     const response = await fetcher(`${baseUrl}/products.json`, 'GET');
     return response.find((item: Products) => item.product_no === Number(id));
   });
+
+  const addCart = () => {
+    console.log({
+      ...product,
+      order: {
+        quantity: quantity,
+        // price: product.price * quantity,
+        date: `${today[0]}-${today[1].padStart(2, '0')}-${today[2].padStart(2, '0')}`,
+      },
+    });
+  };
 
   return (
     <section>
@@ -25,19 +54,34 @@ const ProductDetail = () => {
               <div>
                 <h4>{product.product_name}</h4>
                 <p>{product.price.toLocaleString()}원</p>
-                <i>예상배송일</i>
+                <i>{handleArrivalDate(product.prev_delivery_times)} 도착 예정</i>
+                <br />
+                <i>예상 배송일 {handleDeliveryTimes(product.prev_delivery_times)}일</i>
+                <button onClick={() => setIsSelect(true)}>구매하기</button>
               </div>
-              <div>
-                <p>{product.product_name}</p>
-                <span>
-                  <button>-</button>
-                  <input type="number" defaultValue={1} />
-                  <button>+</button>
-                </span>
-              </div>
-              <div>
-                <button>장바구니에 담기</button>
-              </div>
+              {isSelect && (
+                <div className={styles.order}>
+                  <div>
+                    <p>{product.product_name}</p>
+                    <span>
+                      <select onChange={(e) => setQuantity(Number(e.target.value))}>
+                        {product.maximum_quantity
+                          ? Array(product.maximum_quantity)
+                              .fill(undefined)
+                              .map((_, i) => <option key={i + 1}>{i + 1}</option>)
+                          : Array(100)
+                              .fill(undefined)
+                              .map((_, i) => <option key={i + 1}>{i + 1}</option>)}
+                      </select>
+                    </span>
+                  </div>
+                  <hr />
+                  <p>{(product.price * quantity).toLocaleString()} 원</p>
+                  <div>
+                    <button onClick={() => addCart()}>장바구니에 담기</button>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
           <div className={styles.desc}>{product.description}</div>
--- src/pages/ProductList/index.tsx
+++ src/pages/ProductList/index.tsx
@@ -1,10 +1,9 @@
 import styles from './products.module.scss';
-import { ReactComponent as AddCartIcon } from '../../assets/shopping-cart-add.svg';
 import { useQuery } from '@tanstack/react-query';
 import fetcher from '../../utilities/fetcher';
 import { Products } from '../../type';
-import { Link } from 'react-router-dom';
 import ProductPreview from '../../components/ProductPreview';
+import { useMemo } from 'react';
 
 export const baseUrl = import.meta.env.VITE_APP_API_URL;
 
@@ -13,9 +12,13 @@ const ProductList = () => {
     return fetcher(`${baseUrl}/products.json`, 'GET');
   });
 
+  const productList = useMemo(() => {
+    return products?.sort((prev: Products, next: Products) => prev.product_no - next.product_no);
+  }, [products]);
+
   return (
     <section className={styles.container}>
-      <div>{products?.map((item: Products) => <ProductPreview item={item} key={item.product_no} />)}</div>
+      <div>{productList?.map((item: Products) => <ProductPreview item={item} key={item.product_no} />)}</div>
     </section>
   );
 };
